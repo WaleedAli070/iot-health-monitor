0ea77d6719a782ff2e99a4bdd92f546dfdda28b2
Waleed Ali <waleedali070@gmail.com>
2020-02-25 02:00:47 +0500

    added typeorm configs

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from 'typeorm';
+import * as config from 'config';
+
+const databaseConfig = config.get('database');
+
+export const typeOrmConfig: ConnectionOptions = {
+  type: databaseConfig.type,
+  database: databaseConfig.database || process.env.DB_NAME,
+  entities: ['dist/**/**.entity{.ts,.js}'],
+  synchronize: databaseConfig.synchronize || process.env.TYPEORM_SYNC,
+  logging: databaseConfig.logging,
+};
